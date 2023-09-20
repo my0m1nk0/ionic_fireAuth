7123f9bb25bd4088378708502cbe00a9cb3f6ec4
Micro Social <myomin@blue-stone.net>
2023-09-20 22:22:09 +0630

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'nsocial-e1ee9',
+    appId: '1:387236037211:web:053e168cb57f98e5abf57e',
+    storageBucket: 'nsocial-e1ee9.appspot.com',
+    apiKey: 'AIzaSyDtVMVgR9seB1yrEODNtpN01zeraky-xbE',
+    authDomain: 'nsocial-e1ee9.firebaseapp.com',
+    messagingSenderId: '387236037211',
+    measurementId: 'G-6SEG7KGZQM',
+  },
   production: false
 };
 
